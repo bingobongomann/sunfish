b923d94d1066be804b73b71c320f57f55aab5d52
Jannik Holmer <HolmerJannik@gmail.com>
2022-04-15 10:37:51 +0200

    readlines()

--- engineTestClassicAra.py
+++ engineTestClassicAra.py
@@ -32,14 +32,12 @@ with subprocess.Popen("./ClassicAra", stdin=subprocess.PIPE, stdout=subprocess.P
     CAin = CrazyAra.stdin
     CAout = CrazyAra.stdout
     CAerr = CrazyAra.stderr
-    CAin.write("uci\n")
-    CAin.write("isready\n")
-    out = None
-    while True:
-        if CAout.readable():
-            out = CAout.readline()
-            if out == "readyok":
-                break
+    out = CAout.readlines()
+    print(out)
+    CAin.write('uci\n')
+    out = CAout.readlines()
+    print(out)
+    
     CAin.write("setoption name Batch_size value 1\n")
     board = chess.Board()
     num_correct = 0
